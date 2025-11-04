a390bfe7e5801f1b064b2732e2886e5a12835079
Muhammad Luthfindi Anugrah Pratama <2300018334@webmail.uad.ac.id>
2025-11-04 13:39:48 +0700

    Update

---
+++ app/src/main/java/com/pmob/baseproj5/Post.kt
@@ -0,0 +1,8 @@
+package com.example.sosmed.model
+
+data class Post(
+    var id: Int,
+    var username: String,
+    var caption: String,
+    var imageUri: String
+)
---
+++ app/src/main/java/com/pmob/baseproj5/StoryAdapter.kt
@@ -0,0 +1,24 @@
+package com.pmob.baseproj5
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.pmob.baseproj5.databinding.ItemStoryBinding
+
+class StoryAdapter(private val list: List<String>) :
+    RecyclerView.Adapter<StoryAdapter.ViewHolder>() {
+
+    inner class ViewHolder(val binding: ItemStoryBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemStoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount() = list.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.storyName.text = list[position]
+    }
+}
